f900ddfdf3c6ef019754c24d2e1dc8d704a9c8cf
Turner Inge <tinge@villanova.edu.>
2025-12-01 20:31:51 -0500

    bottomnav shows your profile pic

--- slices/src/components/BottomNav.jsx
+++ slices/src/components/BottomNav.jsx
@@ -1,7 +1,42 @@
 import { NavLink } from 'react-router-dom'
+import { useState, useEffect, use } from "react";
+import { auth, db } from "../scripts/firebase.js";
+import { doc, onSnapshot } from "firebase/firestore";
+
 import '../index.css';
 import './BottomNav.css';
 export default function BottomNav() {
+  const[user, setUser] = useState(null);
+  const[imgUrl, setImgUrl] = useState(null);
+
+useEffect(() => {
+  const unsubscribeAuth = auth.onAuthStateChanged((u) => {
+    setUser(u || null);
+  });
+
+  return () => {
+    unsubscribeAuth();
+  };
+}, []);
+
+useEffect(() => {
+  if (!user) return;
+
+  const userRef = doc(db, "UserAccounts", user.uid);
+
+  const unsubscribeSnap = onSnapshot(userRef, (snap) => {
+    if (snap.exists()) {
+      const data = snap.data();
+      if (data.ProfilePic !== undefined) {
+        setImgUrl(data.ProfilePic);
+      }
+    }
+  });
+
+  return () => {
+    unsubscribeSnap();
+  };
+}, [user]);
   return (
     <nav className="bottom-nav" aria-label="Main navigation">
       <NavLink to="/" end>
@@ -27,7 +62,7 @@ export default function BottomNav() {
       <NavLink to="/profile">
         {({ isActive }) => (
           <img
-            src="../assets/icon.png"
+            src={imgUrl || "../assets/icon.png"}
             alt="Profile"
             className={`nav-icon profile-icon${isActive ? ' active' : ''}`}
           />
